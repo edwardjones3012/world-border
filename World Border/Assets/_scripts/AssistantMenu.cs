1749baba48076358e350db3e024be172e27b83bb
Dominic Brooks <51772450+DominicABrooks@users.noreply.github.com>
2022-12-14 12:54:02 -0600

    Update AssistantMenu.cs
    Fix spacing in MenuItem

--- World Border/Assets/_scripts/AssistantMenu.cs
+++ World Border/Assets/_scripts/AssistantMenu.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class AssistantMenu : EditorWindow
 {
-    [MenuItem("Edit/ Clear Player Prefs")]
+    [MenuItem("Edit / Clear Player Prefs")]
     public static void ClearPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
